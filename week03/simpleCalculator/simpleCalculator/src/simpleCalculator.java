c20e83a3bd1fb0500500f0c434d759937fb31226
jackpods <>
2023-02-18 13:09:16 +0900

    Extract Method를 하면 textfield가 딸려 들어간다.textfiedl가 지역변수이기 ㄱ떄문.

--- week03/simpleCalculator/simpleCalculator/src/simpleCalculator.java
+++ week03/simpleCalculator/simpleCalculator/src/simpleCalculator.java
@@ -2,6 +2,7 @@
 *   2.Jpanle -> 일정한 부분 따로 만들 수 있음.
 *   3.나머지 구하기 (%) => x % 10 =>0~9사이 수
 *   4.정수를 문자열로 변환:  Integer.toString(숫자)
+*   5.중복 발견 -> 중복 제거 =>메서드로! (Extract Method)
 * */
 import javax.swing.*;
 import java.awt.*;
@@ -18,11 +19,9 @@ public class simpleCalculator {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
-
         JTextField textField = new JTextField(10);
         //맨위에 입력하는 창을 만들지만 입력은  못하게 한다.
-        textField.setText(Integer.toString(currentNumber));
+        textField.setText(Integer.toString(currentNumber));//Extract Method를 하면 textfield가 딸려 들어간다.textfiedl가 지역변수이기 ㄱ떄문.
         textField.setEditable(false);
         //계산기 오른쪽에 0이 나오게
         textField.setHorizontalAlignment(JTextField.RIGHT);
