3d157e81f48387b657caba8e57205eac3364558e
jackpods <>
2023-02-18 15:39:49 +0900

    외관과 핵심 능력을 나눔

--- week03/simpleCalculator_practice/Calculator/src/Calculator.java
+++ week03/simpleCalculator_practice/Calculator/src/Calculator.java
@@ -9,21 +9,23 @@
 //  - SCREAMING_SNAKE_CASE -> 전부 대문자, 단어 연결부를 밑줄로 나눔.
 //  - PascalCase -> camelCase의 특수한 형태. 첫 글자가 대문자.
 //                 -> Class 이름에 사용. HelloWorld
+//관심사의 분리를 아주 극단적으로.
+// 클래스 단위로 분리!
+//상태는 볼 수 없다(가시성). 행위(메소드)는 볼 수 있다.
+//꼭 상태를 알고 싶다면 ,getter를 만들어 써라(추천은 아니지만 관례)
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Calculator {
-    //상수
-    private static final String[] OPERATORS = new String[]{"+","-","*","/","="};
 
-    //프로그램의 핵심 상태
+    //GUI 요소
     private JTextField textField;
     private JPanel panel;
 
-    //GUI 요소
-    private long currentNumber = 0;
-    private long accumulator = 0;
-    private String currentOperator = "";
+    //핵심 프로그램 요소  -> 상태에서 객체로 분리가 됐다.
+    private CoreCalculator coreCalculator;
+
 
     //Entry point(객체와 무관하게 따로 존재)
     public static void main(String[] args){
@@ -32,12 +34,15 @@ public class Calculator {
     }
 
     private void run() {
+        coreCalculator = new CoreCalculator();
+
+
         JFrame frame =new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
         textField = new JTextField(10);
-        updateDisplay(currentNumber);
+        updateDisplay(coreCalculator.getCurrentNumber());
         textField.setEditable(false);
          textField.setHorizontalAlignment(JTextField.RIGHT);
         frame.add(textField, BorderLayout.PAGE_START);
@@ -58,8 +63,8 @@ public class Calculator {
             int number = (i+1)%10;
             JButton button = new JButton(Integer.toString(number));
             button.addActionListener(event ->{
-                addnumber(number);
-                updateDisplay(currentNumber);//인자 인수
+                coreCalculator.addnumber(number);
+                updateDisplay(coreCalculator.getCurrentNumber());//인자 인수
             });
             panel.add(button);
         }
@@ -67,40 +72,22 @@ public class Calculator {
 
     public void initOperatorButtons() {
 
-        for(String operator : OPERATORS) {
+        for(String operator :CoreCalculator.OPERATORS) {
             JButton button = new JButton(operator);
             button.addActionListener(event -> {
-                calculate();
-                updateOperator(operator);
-                updateDisplay(accumulator);  //accumlator라는게 인자가 돼서 number라는 매개변수로 받아서 사용.
-
+                coreCalculator.calculate();
+                coreCalculator.updateOperator(operator);
+                updateDisplay(coreCalculator.getAccumulator());  //accumlator라는게 인자가 돼서 number라는 매개변수로 받아서 사용.
             });
             panel.add(button);
         }
     }
 
-    public void calculate( ) {
-        switch (currentOperator) {
-            case "" -> accumulator = currentNumber;
-            case "+" -> accumulator += currentNumber;
-            case "-" -> accumulator -= currentNumber;
-            case "*" -> accumulator *= currentNumber;
-            case "/" -> accumulator /= currentNumber;
-        }
-        currentNumber=0;
-    }
-
-
-    //중복되는 값들만 넣어주려고 할 때
     public void updateDisplay(long number){//파라미터 매개변
         textField.setText(Long.toString(number));
     }
 
-    public void addnumber(int number) {
-        currentNumber *= 10;
-        currentNumber += number;
-    }
-    public void updateOperator(String operator) {
-        currentOperator = operator;
-    }
+
+    //중복되는 값들만 넣어주려고 할 때
+
 }
