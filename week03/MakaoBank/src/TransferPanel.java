1feab6c99872514dc08d5c3d4f9f52954e8790e1
jackpods <>
2023-02-19 17:31:48 +0900

    송금하기 버튼 만들기

--- week03/MakaoBank/src/TransferPanel.java
+++ week03/MakaoBank/src/TransferPanel.java
@@ -1,11 +1,17 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class TransferPanel extends JPanel {
     TransferPanel(){  //같은 이름 써주면 생성자가 된다.
+        setLayout(new GridLayout(3,1));
+
         JLabel label = new JLabel("송금 하기");
         this.add(label);
 
         JTextField textField = new JTextField();
         this.add(textField);
+
+        JButton button = new JButton("송금");
+        this.add(button);
     }
 }
