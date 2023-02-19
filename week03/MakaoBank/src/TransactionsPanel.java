41caa71d0db248a0d0a61d15a72b75387261286b
jackpods <>
2023-02-19 18:21:23 +0900

    이렇게 하거나 인라인한다.

--- week03/MakaoBank/src/TransactionsPanel.java
+++ week03/MakaoBank/src/TransactionsPanel.java
@@ -2,12 +2,12 @@ import javax.swing.*;
 
 public class TransactionsPanel extends JPanel {
     TransactionsPanel(Account account){  //생성자가 됐다.
-        JLabel label = new JLabel("거래 내역");
-        this.add(label);
+        JLabel titleLabel = new JLabel("거래 내역");
+        this.add(titleLabel);
 
         for(String transaction : account.getTransactions()){
-            JLabel transactionLabel = new JLabel(transaction);
-            this.add(transactionLabel);
+            JLabel label = new JLabel(transaction);
+            this.add(label);
         }
     }
 }
