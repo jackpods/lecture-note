ec8a1f14fe94c9892079b47a6cf3f24a1075e37b
jackpods <>
2023-02-19 18:18:55 +0900

    정리

--- week03/MakaoBank/src/Account.java
+++ week03/MakaoBank/src/Account.java
@@ -6,7 +6,7 @@ public class Account {
 
     private long amount = 1000;//잔액
 
-    private List<String> transactions  ;//거래 내역  =부터 생성자에서 만들어줘도됨.
+    private List<String> transactions = new ArrayList<>() ;//거래 내역  =부터 생성자에서 만들어줘도됨.
 
     public String getNumber(){
         return number;
@@ -22,7 +22,6 @@ public class Account {
         amount -= transferamount;
 
         //거래내력을 기록해줌. Transfer에서 할 필요 없음
-        transactions = new ArrayList<>();
         transactions.add("돈 보냈다~");  //이대로면 transactions가 지역변수라 실행하고나면 끝이다 그러니 필드로 보내야한다.
     }
 
