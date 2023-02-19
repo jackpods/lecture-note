2c079c154b20c3eb58c255ab7dd2bc627e5b170c
jackpods <>
2023-02-19 18:20:34 +0900

    이렇게 하거나

--- week03/MakaoBank/src/TransactionsPanel.java
+++ week03/MakaoBank/src/TransactionsPanel.java
@@ -4,5 +4,10 @@ public class TransactionsPanel extends JPanel {
     TransactionsPanel(Account account){  //생성자가 됐다.
         JLabel label = new JLabel("거래 내역");
         this.add(label);
+
+        for(String transaction : account.getTransactions()){
+            JLabel transactionLabel = new JLabel(transaction);
+            this.add(transactionLabel);
+        }
     }
 }
