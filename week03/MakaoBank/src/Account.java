38a835b831b7e176d88aafed5d973270d433274e
jackpods <>
2023-02-19 18:37:20 +0900

    복사본  인라인으로 변경하면

--- week03/MakaoBank/src/Account.java
+++ week03/MakaoBank/src/Account.java
@@ -19,7 +19,8 @@ public class Account {
 
     //getter;
     public List<String> getTransactions() {
-        return transactions;
+         List<String> copy = new ArrayList<>(transactions);  //사이즈가 안맞아서 이상하게 작동 될 수 있다. 그래서 실제로 돌려준 것과 간직하고 있는게 다르게 하기 위해서 복사본을 준다.
+        return copy;
     }
 
     //변형 getter! -> 이런 형태를 권장함.
--- week03/MakaoBank/src/TransactionsPanel.java
+++ week03/MakaoBank/src/TransactionsPanel.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.List;
 
 public class TransactionsPanel extends JPanel {
     TransactionsPanel(Account account){  //생성자가 됐다.
         this.setLayout(new GridLayout(1+account.transactionsSize(), 1));
 
         this.add(new JLabel("거래 내역"));
+
+        List<String> transactions = account.getTransactions();
+        transactions.add("해킹");
 
         for(String transaction : account.getTransactions()){
             this.add(new JLabel(transaction));
